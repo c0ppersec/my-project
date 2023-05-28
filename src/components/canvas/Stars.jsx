9f194026f35cc4aa343fe1a0885c9dbb436900e9
c0ppersec <xvanz.id@gmail.com>
2023-05-28 17:13:40 +0700

    editing my project

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -31,11 +31,11 @@ const StarsCanvas = () => {
   return (
     <div className='w-full h-auto absolute inset-0 z-[-1]'>
       <Canvas camera={{ position: [0, 0, 1] }}>
-        {/* <Suspense fallback={null}> */}
+        <Suspense fallback={null}>
           <Stars />
-        {/* </Suspense> */}
+        </Suspense>
 
-        {/* <Preload all /> */}
+        <Preload all />
       </Canvas>
     </div>
   );
