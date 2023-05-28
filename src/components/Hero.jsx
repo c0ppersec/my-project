9ed4b0321512c85faa48137a05da0f0c71ee7280
c0ppersec <xvanz.id@gmail.com>
2023-05-28 08:39:04 +0700

    editing my project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,11 +19,11 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText}sm:flex text-white 100 md:max-lg:flex`}>
             We're<span className='text-[#FFFFFF]'> Walk Art Creative!</span>
           </h1>
-          <p className={`${styles.heroSubText} ml-1 text-transparent text-1xl bg-clip-text bg-gradient-to-r from-pink-400 to-purple-600 animate-pulse`}>
+          <p className={`${styles.heroSubText} mt-10 ml-1 text-transparent text-1xl bg-clip-text bg-gradient-to-r from-pink-400 to-purple-600 animate-pulse`}>
            We bring your imagination to life <br className='sm:block hidden' />
           </p>
           {/* <p className='sm:flex flex-row mt-32 md:max-lg md:mt-32 md:ml-72 md:pl-96 font-normal text-transparent text-1xl bg-clip-text bg-gradient-to-r from-lime-50 to-pink-700 text-[17.5px] max-1xl leading-[19px]'></p> */}
-          <p className='sm:flex flex-row mt-28 md:max-lg md:mt-40 md:ml-72 md:pl-96 text-transparent text-1xl bg-clip-text bg-gradient-to-r from-lime-50 to-pink-700'>
+          <p className='sm:flex flex-row mt-3 md:max-lg md:mt-40 md:ml-72 md:pl-96 text-transparent text-1xl bg-clip-text bg-gradient-to-r from-lime-50 to-pink-700'>
           With experience in various industries, we provide creative digital solutions to <br />
           help you achieve your goals.
 
@@ -34,11 +34,11 @@ const Hero = () => {
       {/* <ComputersCanvas /> */}
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-      <p className="xs:flex-auto mt-10 sm:flex sm:mb-3 md:mb-10 font-medium text-transparent text-2xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+      <p className="xs:flex mt-28 sm:flex mb-3 md:mb-10 font-medium text-transparent text-2xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
           Get to know more &nbsp; 
         </p>
         <a href='#about'>
-          <div className='xs:flex-auto mt-10 sm:flex sm:mb-3 md:mb-10 w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+          <div className='xs:flex mt-28 sm:flex mb-3 md:mb-10 w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{
                 y: [0, 24, 0],
